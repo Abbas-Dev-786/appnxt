a43068567028b24949d860455f45787e24b1456c
Abbas Bhanpura wala <abbasbhanpura.dev@gmail.com>
2025-04-18 09:38:59 +0530

    feat: created home banner Img API

--- server/controllers/HomeBannerController.js
+++ server/controllers/HomeBannerController.js
@@ -1,50 +1,95 @@
-const route = require('express').Router()
-const homeModel = require('../models/HomeSchema')
+const route = require("express").Router();
+const homeModel = require("../models/HomeSchema");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+const { s3Client } = require("../utils/aws");
+const path = require("path");
 
-route.get('/', async (req, res) => {
-    try {
-        const homeData = await homeModel.findOne({}, { banner: 1, _id: 0 });
-
-        if (!homeData) {
-            return res.status(404).send({ success: false, message: 'No banner data found' });
-        }
-        console.log(homeData)
-
-        return res.status(200).send({ success: true, data: homeData });
-    } catch (error) {
-        console.error('Error fetching banner data:', error);
-        return res.status(500).send({ success: false, message: 'Internal Server Error' });
-    }
+// Multer S3 storage configuration
+const storage = multerS3({
+  s3: s3Client,
+  bucket: process.env.AWS_BUCKET_NAME,
+  // acl: 'public-read',
+  metadata: (req, file, cb) => {
+    cb(null, { fieldName: file.fieldname });
+  },
+  key: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const extension = path.extname(file.originalname);
+    const newFilename = `projects/${uniqueSuffix}${extension}`;
+    cb(null, newFilename); // S3 key (path within the bucket)
+  },
 });
 
-route.get('/:id', async(req, res) => {
+// Multer instance for handling file uploads
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 100 * 1024 * 1024 }, // Set file size limit to 10 MB
+  fileFilter: (req, file, cb) => {
+    cb(null, true);
+  },
+}).any();
 
-})
+route.get("/", async (req, res) => {
+  try {
+    const homeData = await homeModel.findOne(
+      {},
+      { banner: 1, bannerImg: 1, _id: 0 }
+    );
 
-route.post('/', async (req, res) => {
-    try {
-        const { banner } = req.body;
-
-        if (!banner) {
-            return res.status(400).send({ success: false, message: 'Empty Data Not Accepted' });
-        }
-
-        await homeModel.updateOne({}, { $set: { banner } }, { upsert: true });
-
-        return res.status(200).send({ success: true });
-    } catch (error) {
-        console.error('Error updating banner:', error);
-        return res.status(500).send({ success: false, message: 'Internal Server Error' });
+    if (!homeData) {
+      return res
+        .status(404)
+        .send({ success: false, message: "No banner data found" });
     }
+    console.log(homeData);
+
+    return res.status(200).send({ success: true, data: homeData });
+  } catch (error) {
+    console.error("Error fetching banner data:", error);
+    return res
+      .status(500)
+      .send({ success: false, message: "Internal Server Error" });
+  }
 });
 
-route.put('/:id', async(req, res) => {
+route.get("/:id", async (req, res) => {});
 
-})
+route.post("/", upload, async (req, res) => {
+  try {
+    const { banner } = req.body;
 
-route.delete('/:id', async(req, res) => {
+    if (!banner) {
+      return res
+        .status(400)
+        .send({ success: false, message: "Empty Data Not Accepted" });
+    }
 
-})
+    const data = {
+      banner,
+    };
 
+    if (req.files.find((file) => file.fieldname === "bannerImg")) {
+      data.bannerImg = {
+        s3Url: req.files.find((file) => file.fieldname === "bannerImg")
+          .location, // URL of the uploaded main banner
+        s3Key: req.files.find((file) => file.fieldname === "bannerImg").key, // Key of the uploaded main banner
+      };
+    }
+
+    await homeModel.updateOne({}, { $set: data }, { upsert: true });
+
+    return res.status(200).send({ success: true });
+  } catch (error) {
+    console.error("Error updating banner:", error);
+    return res
+      .status(500)
+      .send({ success: false, message: "Internal Server Error" });
+  }
+});
+
+route.put("/:id", async (req, res) => {});
+
+route.delete("/:id", async (req, res) => {});
 
 module.exports = route;
--- server/models/HomeSchema.js
+++ server/models/HomeSchema.js
@@ -1,11 +1,15 @@
-require('../config/dataBase')
-const mongoose = require('mongoose')
+require("../config/dataBase");
+const mongoose = require("mongoose");
 
+const homeSchema = new mongoose.Schema(
+  {
+    banner: { type: String, default: "" },
+    bannerImg: {
+      s3Url: { type: String, default: "" },
+      s3Key: { type: String, default: "" },
+    },
+  },
+  { collection: "homeData" }
+);
 
-const homeSchema = new mongoose.Schema({
-
-banner: { type: String, default: '' }
-
-}, { collection : "homeData" });
-
-module.exports = mongoose.model('homeData', homeSchema);  
+module.exports = mongoose.model("homeData", homeSchema);
