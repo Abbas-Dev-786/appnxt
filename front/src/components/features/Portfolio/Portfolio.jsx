6881518b86cf59b93b90d5166e356d4935742887
Abbas Bhanpura wala <abbasbhanpura.dev@gmail.com>
2025-04-18 11:55:27 +0530

    feat: integrated banner API's on admin panel

--- front/src/components/features/Portfolio/Portfolio.jsx
+++ front/src/components/features/Portfolio/Portfolio.jsx
@@ -1,32 +1,45 @@
-import { useScrollToTop } from "../../../util/scrollHook"
-import Banner from "../../shared/Banner/Banner"
-import Footer from "../../shared/Footer/Footer"
-import Header from "../../shared/Header/Header"
-import Projects from "../../shared/Projects/Projects"
+import { useSelector } from "react-redux";
+import { useScrollToTop } from "../../../util/scrollHook";
+import Banner from "../../shared/Banner/Banner";
+import Footer from "../../shared/Footer/Footer";
+import Header from "../../shared/Header/Header";
+import Projects from "../../shared/Projects/Projects";
 
 const Portfolio = () => {
+  const bannerHeading = useSelector((state) => state.BannerDataSlice);
+  console.log(bannerHeading.data.bannerImg.s3Url);
 
   const projectHeading = {
     highlightSize: window.innerWidth >= 767 ? 88 : 45,
-    normal: 'Projects!',
+    normal: "Projects!",
     normalSize: window.innerWidth >= 767 ? 78 : 40,
-    highlight: 'Featured WOW',
-    description: 'Explore our collection of cutting-edge products designed to empower your business and elevate your creative potential.Each product is meticulously crafted to provide exceptional performance, usability, and results.'
-  }
+    highlight: "Featured WOW",
+    description:
+      "Explore our collection of cutting-edge products designed to empower your business and elevate your creative potential.Each product is meticulously crafted to provide exceptional performance, usability, and results.",
+  };
 
-  useScrollToTop()
+  useScrollToTop();
 
   return (
     <>
-        <Header />
+      <Header />
 
-        <Banner content={'APPNXT PVT. LTD. delivers custom software solutions, using advanced technologies to create robust, tailored products for your business.'} heading={'Projects'} bgURL={'/assets/img/portfolio-banner.svg'} />
+      <Banner
+        content={
+          "APPNXT PVT. LTD. delivers custom software solutions, using advanced technologies to create robust, tailored products for your business."
+        }
+        heading={"Projects"}
+        bgURL={
+          bannerHeading?.data?.bannerImg?.s3Url ||
+          "/assets/img/portfolio-banner.svg"
+        }
+      />
 
-        <Projects type={'center'} content={projectHeading} />
+      <Projects type={"center"} content={projectHeading} />
 
-        <Footer />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
--- front/src/modules/RootModule.jsx
+++ front/src/modules/RootModule.jsx
@@ -1,25 +1,39 @@
-import { useEffect } from 'react'
-import { Outlet, useNavigate } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
-import { fetchSlider } from '../services/SliderService'
-import { fetchWorkProcess } from '../services/WorkProcessService'
-import { fetchProjects } from '../services/ProjectService'
-import { fetchCategory, fetchServices } from '../services/ServiceService'
-import { fetchHome } from '../services/HomeService'
-import { fetchCounter } from '../services/CounterService'
-import { fetchWhatWeDo } from '../services/WhatWeDoService'
-import { handleFetchData as sliderRedux } from '../redux/SliderDataSlice'
-import { handleFetchData as workProcessRedux } from '../redux/WorkProcessDataSlice'
-import { handleFetchData as projectRedux } from '../redux/ProjectDataSlice'
-import { handleFetchCategory, handleFetchData as serviceRedux } from '../redux/ServiceDataSlice'
-import { handlePostBanner, handlePostCounter, handlePostWhatWeDo } from '../redux/AdminDataSlice'
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { fetchSlider } from "../services/SliderService";
+import { fetchWorkProcess } from "../services/WorkProcessService";
+import { fetchProjects } from "../services/ProjectService";
+import { fetchCategory, fetchServices } from "../services/ServiceService";
+import { fetchHome } from "../services/HomeService";
+import { fetchCounter } from "../services/CounterService";
+import { fetchWhatWeDo } from "../services/WhatWeDoService";
+import { handleFetchData as sliderRedux } from "../redux/SliderDataSlice";
+import { handleFetchData as workProcessRedux } from "../redux/WorkProcessDataSlice";
+import { handleFetchData as projectRedux } from "../redux/ProjectDataSlice";
+import {
+  handleFetchCategory,
+  handleFetchData as serviceRedux,
+} from "../redux/ServiceDataSlice";
+import {
+  handlePostBanner,
+  handlePostCounter,
+  handlePostWhatWeDo,
+} from "../redux/AdminDataSlice";
+import {
+  getPortfolioBanner,
+  getServiceBanner,
+} from "../services/BannerService";
+import {
+  handleFetchPortfolioBannerData,
+  handleFetchServiceBannerData,
+} from "../redux/BannerDataSlice";
 
 const RootModule = () => {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   useEffect(() => {
-
     // Individual fetch functions with error handling
     const fetchAndDispatchData = async (fetchFn, dispatchFn) => {
       try {
@@ -57,14 +71,16 @@ const RootModule = () => {
 
       // What we do data
       fetchAndDispatchData(fetchWhatWeDo, handlePostWhatWeDo);
+
+      // banner data
+      fetchAndDispatchData(getServiceBanner, handleFetchServiceBannerData);
+      fetchAndDispatchData(getPortfolioBanner, handleFetchPortfolioBannerData);
     };
 
     fetchAllData();
   }, [dispatch, navigate]);
 
-  return (
-        <Outlet />
-  );
+  return <Outlet />;
 };
 
 export default RootModule;
